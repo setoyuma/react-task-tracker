8b6a98741293454fa3a4a89847f74e111c2a6daf
seto yuma <setoichi.dev@gmail.com>
2023-01-10 02:18:30 -0800

    inital commit
    simple task tracking app

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer>
+        <p>yumaden &copy; 2023</p>
+        <a href="https://www.github.com/setoyuma">Github</a>
+        {/* <a href='/about'>About</a> */}
+    </footer>
+  )
+}
+
+export default Footer
